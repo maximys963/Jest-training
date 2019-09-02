2e75b72ed7f3e729eabf782bafb97377adbbebcd
maximys963 <maxim.p.jsdev@gmail.com>
2019-09-02 21:33:16 +0300

    simple counter

--- simple_app/src/App.js
+++ simple_app/src/App.js
@@ -1,13 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component }  from 'react';
+import { Button }            from 'antd'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World !!!
-    </div>
-  );
+class App extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter : 0
+        }
+    }
+
+    handleIncreaseCounter = () => {
+        this.setState({ counter: this.state.counter + 1 });
+    };
+
+    handleDecreaseCounter = () => {
+        this.setState({ counter: this.state.counter - 1 });
+    };
+
+    handleEraseCounter = () => {
+        this.setState({ counter: 0 });
+    };
+
+    render(){
+        const { counter } = this.state;
+        return(
+            <div className='app-container'>
+                <h1>
+                    Counter Value now is { counter }
+                </h1>
+                <Button
+                    className='app-button'
+                    onClick={this.handleIncreaseCounter}
+                >
+                    Increase counter
+                </Button>
+                <Button
+                    className='app-button'
+                    onClick={this.handleDecreaseCounter}
+                >
+                    Decrease counter
+                </Button>
+                <Button
+                    className='app-button'
+                    onClick={this.handleEraseCounter}
+                >
+                    Erase
+                </Button>
+            </div>
+        )
+    }
 }
 
 export default App;
