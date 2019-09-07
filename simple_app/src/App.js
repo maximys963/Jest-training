c12f0c70a3c5aa82cad4cb9bd83787fbda493714
maximys963 <maxim.p.jsdev@gmail.com>
2019-09-07 19:33:53 +0300

    finish first unit

--- simple_app/src/App.js
+++ simple_app/src/App.js
@@ -1,14 +1,43 @@
 import React, { Component }  from 'react';
-import { Button }            from 'antd'
+import {
+    Button,
+    Modal
+} from 'antd'
 import './App.css';
 
 class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            counter : 0
+            counter : 0,
+            visible: false
         }
     }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        const { counter } = this.state;
+        if(counter < 0) this.validateCounterValue();
+    }
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleOk = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
 
     handleIncreaseCounter = () => {
         this.setState({ counter: this.state.counter + 1 });
@@ -22,31 +51,51 @@ class App extends Component{
         this.setState({ counter: 0 });
     };
 
-    render(){
+    validateCounterValue = () => {
         const { counter } = this.state;
+        if(counter < 0 ) {
+            this.setState({ counter: 0 });
+            this.showModal();
+        }
+    };
+
+    render(){
+        const {
+            counter,
+            visible
+        } = this.state;
+
         return(
             <div className='app-container' data-test="something">
                 <h1 className='counter-display'>
                     Counter Value now is { counter }
                 </h1>
                 <Button
-                    className='app-button'
+                    className='increase-btn'
                     onClick={this.handleIncreaseCounter}
                 >
                     Increase counter
                 </Button>
                 <Button
-                    className='app-button'
+                    className='decrease-btn'
                     onClick={this.handleDecreaseCounter}
                 >
                     Decrease counter
                 </Button>
                 <Button
-                    className='app-button'
+                    className='erase-btn'
                     onClick={this.handleEraseCounter}
                 >
                     Erase
                 </Button>
+                <Modal
+                    title="Error"
+                    visible={visible}
+                    onOk={this.handleOk}
+                    onCancel={this.handleCancel}
+                >
+                    <p>Counter can't be lower then 0</p>
+                </Modal>
             </div>
         )
     }
--- simple_app/src/App.test.js
+++ simple_app/src/App.test.js
@@ -6,28 +6,97 @@ import App            from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+/**
+ *
+ * Factory function to create a ShallowWrapper for the App component-app.
+ * @function setup
+ * @params {object} props - Component props specific for this setup.
+ * @params {object} state - initial state for setup.
+ * @returns {ShallowWrapper}
+ */
+
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+
+};
+
+/**
+ * Return ShallowWrapper containing node(s) with given data-test value.
+ * @param {ShallowWrapper} wrapper  - Enzyme shallow wrapper to search within.
+ * @param {string} val - Value of data-test attribute for search.
+ * @return {ShallowWrapper }
+ */
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[className="${val}"]`)
+
+};
+
 test('renders without crashing', () => {
-  const wrapper = shallow(<App/>);
-  const appComponent = wrapper.find("[data-test='something']");
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'app-container');
   expect(appComponent.length).toBe(1);
 });
 
 test('renders increment button', () => {
-  const wrapper = shallow(<App/>);
-  const button = wrapper.find("[className='app-button']");
-  expect(button.length).toBe(3);
+  const wrapper = setup();
+  const button =findByTestAttr(wrapper, 'increase-btn');
+  expect(button.length).toBe(1);
 });
 
 test('renders counter display', () => {
-  const wrapper = shallow(<App/>);
-  const counterDisplay = wrapper.find("[className='counter-display']");
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
   expect(counterDisplay.length).toBe(1);
 });
 
 test('counter starts at zero', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
 
 });
 
 test('clicking the button increment the counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
 
+  //find button and click
+  const button =findByTestAttr(wrapper, 'increase-btn');
+  button.simulate('click');
+  wrapper.update();
+
+  //find counterDisplay and test value
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
+  expect(counterDisplay.text()).toContain( counter + 1);
+});
+
+test('clicking the button decrement the counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  //find button and click
+  const button =findByTestAttr(wrapper, 'decrease-btn');
+  button.simulate('click');
+  wrapper.update();
+
+  //find counterDisplay and test value
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
+  expect(counterDisplay.text()).toContain( counter - 1);
+});
+
+test('clicking the button erse the counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+
+  //find button and click
+  const button =findByTestAttr(wrapper, 'erase-btn');
+  button.simulate('click');
+  wrapper.update();
+
+  //find counterDisplay and test value
+  const counterDisplay = findByTestAttr(wrapper,'counter-display');
+  expect(counterDisplay.text()).toContain(0);
 });
