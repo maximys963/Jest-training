04db286ee437fb6c0c1ebf8debc718f41b44b812
maximys963 <maxim.p.jsdev@gmail.com>
2019-12-30 18:15:20 +0200

    write first tests

--- simple_app/src/App.test.js
+++ simple_app/src/App.test.js
@@ -1,8 +1,7 @@
-import React          from 'react';
-import Enzyme,
-{ shallow }           from 'enzyme';
-import EnzymeAdapter  from 'enzyme-adapter-react-16'
-import App            from './App';
+import React               from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter       from 'enzyme-adapter-react-16'
+import App                 from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
