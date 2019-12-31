f2c07890c2ae28dee30039f572126359d3a4907c
maximys963 <maxim.p.jsdev@gmail.com>
2019-12-31 17:13:54 +0200

    move generic configs to setupTest.js

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -1,7 +1,6 @@
-import React               from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import EnzymeAdapter       from 'enzyme-adapter-react-16';
-import checkPropTypes      from 'check-prop-types';
+import React       from 'react';
+import { shallow } from 'enzyme';
+
 
 import {
     findByTestAttr,
@@ -9,7 +8,6 @@ import {
 }                          from "../test/testUtils";
 import Congrats            from './Congrats';
 
-Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 const defaultProps = { success: false };
 
