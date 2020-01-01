f50d9f7b173fb57b975c1154b0a73fd1ff90d55e
maximys963 <maxim.p.jsdev@gmail.com>
2020-01-01 19:08:53 +0200

    add some more cover tests to GuessWord component

--- jotto/src/components/GuessedWords/GuessedWords.js
+++ jotto/src/components/GuessedWords/GuessedWords.js
@@ -9,6 +9,34 @@ const GuessedWords = (props) => {
                 Try to guess the secret world!
             </span>
         )
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => (
+            <tr
+                data-test = "guessed-word"
+                key       = {index}
+            >
+                <td>{word.guessedWords}</td>
+                <td>{word.letterMatchCount}</td>
+            </tr>
+        ));
+
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Matching Letters</th>
+                        </tr>
+                    </thead>
+                   <tbody>
+                   { guessedWordsRows }
+                   </tbody>
+                </table>
+
+            </div>
+        )
     }
     return(
         <div data-test="component-guessed-words">
