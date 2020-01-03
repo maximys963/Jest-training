fa6737b91845a6763e63e3ee2615d4b379c93747
maximys963 <maxim.p.jsdev@gmail.com>
2020-01-03 21:31:56 +0200

    add test for action

--- jotto/src/index.js
+++ jotto/src/index.js
@@ -1,12 +1,12 @@
-import React        from 'react';
-import ReactDOM     from 'react-dom';
-import { Provider } from 'react-redux';
+import React              from 'react';
+import ReactDOM           from 'react-dom';
+import { Provider }       from 'react-redux';
 
 
 import './index.css';
-import App from './App';
+import App                from './App';
 import * as serviceWorker from './serviceWorker';
-import store from './configureStore';
+import store              from './configureStore';
 
 
 
